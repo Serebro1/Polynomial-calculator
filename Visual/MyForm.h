3055553d232cab6d5259949a5124ff3d9c9f6a5e
Serebro1 <OtcheskovSemen@yandex.ru>
2025-03-08 15:32:06 +0300

    visual layout

--- Visual/MyForm.h
+++ Visual/MyForm.h
@@ -35,6 +35,37 @@ namespace Visual {
 			}
 		}
 
+	protected:
+
+	protected:
+	private: System::Windows::Forms::SplitContainer^ splitContainer1;
+	private: System::Windows::Forms::SplitContainer^ splitContainer2;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel1;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel2;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::ListBox^ polinomListBox;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel3;
+	private: System::Windows::Forms::Panel^ panel2;
+
+
+	protected:
+
+
+
+	protected:
+
+
+
+	protected:
+
+
+
+
+
+
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
@@ -48,11 +79,176 @@ namespace Visual {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->splitContainer2 = (gcnew System::Windows::Forms::SplitContainer());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->polinomListBox = (gcnew System::Windows::Forms::ListBox());
+			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel2 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel3 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->BeginInit();
+			this->splitContainer1->Panel1->SuspendLayout();
+			this->splitContainer1->Panel2->SuspendLayout();
+			this->splitContainer1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer2))->BeginInit();
+			this->splitContainer2->Panel1->SuspendLayout();
+			this->splitContainer2->Panel2->SuspendLayout();
+			this->splitContainer2->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			this->flowLayoutPanel1->SuspendLayout();
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this->splitContainer1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->splitContainer1->Location = System::Drawing::Point(0, 484);
+			this->splitContainer1->Name = L"splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this->splitContainer1->Panel1->Controls->Add(this->flowLayoutPanel1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this->splitContainer1->Panel2->Controls->Add(this->splitContainer2);
+			this->splitContainer1->Size = System::Drawing::Size(404, 150);
+			this->splitContainer1->SplitterDistance = 110;
+			this->splitContainer1->TabIndex = 1;
+			// 
+			// splitContainer2
+			// 
+			this->splitContainer2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer2->Location = System::Drawing::Point(0, 0);
+			this->splitContainer2->Name = L"splitContainer2";
+			// 
+			// splitContainer2.Panel1
+			// 
+			this->splitContainer2->Panel1->Controls->Add(this->flowLayoutPanel2);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this->splitContainer2->Panel2->Controls->Add(this->flowLayoutPanel3);
+			this->splitContainer2->Size = System::Drawing::Size(290, 150);
+			this->splitContainer2->SplitterDistance = 173;
+			this->splitContainer2->TabIndex = 0;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->polinomListBox);
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox1->Location = System::Drawing::Point(0, 0);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(404, 484);
+			this->groupBox1->TabIndex = 2;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"History";
+			// 
+			// polinomListBox
+			// 
+			this->polinomListBox->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->polinomListBox->FormattingEnabled = true;
+			this->polinomListBox->Location = System::Drawing::Point(3, 16);
+			this->polinomListBox->Name = L"polinomListBox";
+			this->polinomListBox->Size = System::Drawing::Size(398, 465);
+			this->polinomListBox->TabIndex = 0;
+			// 
+			// flowLayoutPanel1
+			// 
+			this->flowLayoutPanel1->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel1->Controls->Add(this->panel1);
+			this->flowLayoutPanel1->Controls->Add(this->panel2);
+			this->flowLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->flowLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
+			this->flowLayoutPanel1->Size = System::Drawing::Size(110, 150);
+			this->flowLayoutPanel1->TabIndex = 0;
+			// 
+			// flowLayoutPanel2
+			// 
+			this->flowLayoutPanel2->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->flowLayoutPanel2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->flowLayoutPanel2->Location = System::Drawing::Point(0, 0);
+			this->flowLayoutPanel2->Name = L"flowLayoutPanel2";
+			this->flowLayoutPanel2->Size = System::Drawing::Size(173, 150);
+			this->flowLayoutPanel2->TabIndex = 0;
+			// 
+			// flowLayoutPanel3
+			// 
+			this->flowLayoutPanel3->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->flowLayoutPanel3->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->flowLayoutPanel3->Location = System::Drawing::Point(0, 0);
+			this->flowLayoutPanel3->Name = L"flowLayoutPanel3";
+			this->flowLayoutPanel3->Size = System::Drawing::Size(113, 150);
+			this->flowLayoutPanel3->TabIndex = 0;
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::SystemColors::Control;
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(3, 3);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(104, 25);
+			this->panel1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->label1->Location = System::Drawing::Point(0, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(42, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Моном";
+			// 
+			// button1
+			// 
+			this->button1->Dock = System::Windows::Forms::DockStyle::Right;
+			this->button1->Location = System::Drawing::Point(78, 0);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(26, 25);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"\?";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			this->panel2->Location = System::Drawing::Point(3, 34);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(200, 100);
+			this->panel2->TabIndex = 1;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(404, 634);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->splitContainer1);
+			this->MinimumSize = System::Drawing::Size(420, 0);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->splitContainer1->Panel1->ResumeLayout(false);
+			this->splitContainer1->Panel2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
+			this->splitContainer2->Panel1->ResumeLayout(false);
+			this->splitContainer2->Panel2->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer2))->EndInit();
+			this->splitContainer2->ResumeLayout(false);
+			this->groupBox1->ResumeLayout(false);
+			this->flowLayoutPanel1->ResumeLayout(false);
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
